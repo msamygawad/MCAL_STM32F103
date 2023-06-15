74259916cf343f583bb3b386ec521e39c92775be
mohamedehab97 <mohamed.ehab2442@gmail.com>
2023-06-15 20:45:52 +0300

    Udated the GPIO driver with more abstraction features

--- _src/gpio/gpio.c
+++ _src/gpio/gpio.c
@@ -1,319 +1,161 @@
+/*****************************************************************************************************
+ * FILE DESCRIPTION
+ * ---------------------------------------------------------------------------------------------------
+ * File name:    GPOI Programme
+ * Module :      GPIO Module
+ * Description:  file contains the static code for GPIO APIs provided
+ * REV:  		1.0
+ * DATE:			2023
+ * Autor: M. Ehab
+ *
+ ****************************************************************************************************/
 
-
-/*
-****************************************************************************************************
-* @file					:	gpio.c
-* @author				:	M.Ehab
-* @brief				:	the file contains static code for GPIO driver for STM32 M3
-****************************************************************************************************
-*/
-
-/******************************** Includes Start **************************************************/
+/******************************** Includes Start ****************************************************/
+#include "interface_gpio.h"
 #include "private_gpio.h"
-/******************************** Includes End ****************************************************/
+/******************************** Includes End ******************************************************/
 
 /******************************** Software Interface Implementation Start ***************************/
-void GPIO_Init_PIN(u32 GPIOx, u32 GPIO_Pin, u8 GPIO_Mode, u8 GPIO_CFG)
+GPIO_ErrorHandler_Type GPIO_Init(GPIO_Config_Type *GPIO_Pin_CFG)
 {
-    /* Enablicg Clock for GPIOx
-        if (GPIOx == PA)
-        {
-            RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
-            RCC->APB2RSTR &= ~(RCC_APB2RSTR_IOPARST);
-        }
-        else if (GPIOx == PB)
-        {
-            RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
-            RCC->APB2RSTR &= ~(RCC_APB2RSTR_IOPBRST);
-        }
-        else if (GPIOx == PC)
-        {
-            RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
-            RCC->APB2RSTR &= ~(RCC_APB2RSTR_IOPCRST);
-        }
-        else if (GPIOx == PD)
-        {
-            RCC->APB2ENR |= RCC_APB2ENR_IOPDEN;
-            RCC->APB2RSTR &= ~(RCC_APB2RSTR_IOPDRST);
-        }
-    */
-    u32 temp_value = 0x00;        // temp value to store the value of the mode and configuration
-    temp_value |= GPIO_CFG;       // ORing the configuration with the temp value
-    temp_value = temp_value << 2; // shifting the value to the left by 2 bits to stor in CNF bits
-    temp_value |= GPIO_Mode;      // ORing the mode with the temp value
-
-    if (GPIO_Pin <= PIN7)
+    // Enablicg Clock for GPIOx
+    /*
+    switch (GPIO_Pin_CFG->GPIO_Port == GPIO_PORTA)
     {
-        temp_value = temp_value << GPIO_CRL_Offset * GPIO_Pin; // shifting the value to the left by 4*pin number to store in the right pin
-        switch (GPIOx)
-        {
-        case PA:
-            GPIOA->CRL |= temp_value;
-            break;
-        case PB:
-            GPIOB->CRL |= temp_value;
-            break;
-        case PC:
-            GPIOC->CRL |= temp_value;
-            break;
-        case PD:
-            GPIOD->CRL |= temp_value;
-            break;
-        }
+    case GPIO_PORTA:
+        RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+        RCC->APB2RSTR &= ~(RCC_APB2RSTR_IOPARST);
+        break;
+    case GPIO_PORTB:
+        RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+        RCC->APB2RSTR &= ~(RCC_APB2RSTR_IOPBRST);
+        break;
+    case GPIO_PORTC:
+        RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+        RCC->APB2RSTR &= ~(RCC_APB2RSTR_IOPCRST);
+        break;
+    case GPIO_PORTD:
+        RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+        RCC->APB2RSTR &= ~(RCC_APB2RSTR_IOPDRST);
+        break;
+
+    default: return GPIO_InputErr;
+        break;
+    }
+       */
+
+    if (!(GPIO_Pin_CFG->GPIO_Pin_Mode >= GPIO_INPUT && GPIO_Pin_CFG->GPIO_Pin_Mode <= GPIO_OUTPUT_50_MHz))
+    {
+        return GPIO_InputErr;
+    }
+    if (!(GPIO_Pin_CFG->GPIO_Pin_Config >= GPIO_PUSH_PULL && GPIO_Pin_CFG->GPIO_Pin_Mode <= GPIO_ALTERNATE_FUNCTION_OPEN_DRAIN))
+    {
+        return GPIO_InputErr;
+    }
+    uint32_t temp_value = 0x00;                  // temp value to store the value of the mode and configuration
+    temp_value |= GPIO_Pin_CFG->GPIO_Pin_Config; // ORing the configuration with the temp value
+    temp_value = temp_value << 2;                // shifting the value to the left by 2 bits to stor in CNF bits
+    temp_value |= GPIO_Pin_CFG->GPIO_Pin_Mode;   // ORing the mode with the temp value
+
+    if (GPIO_Pin_CFG->GPIO_Pin <= GPIO_PIN7 && GPIO_Pin_CFG->GPIO_Pin >= GPIO_PIN0)
+    {
+        temp_value = temp_value << GPIO_CRL_Offset * GPIO_Pin_CFG->GPIO_Pin; // shifting the value to the left by 4*pin number to store in the right pin
+        GPIO_CRL(GPIO_Pin_CFG->GPIO_Port) |= temp_value;                     // ORing the value with the right register
+    }
+    else if (GPIO_Pin_CFG->GPIO_Pin <= GPIO_PIN15 && GPIO_Pin_CFG->GPIO_Pin >= GPIO_PIN8)
+    {
+        temp_value = temp_value << GPIO_CRL_Offset * (GPIO_Pin_CFG->GPIO_Pin - 8);
+        GPIO_CRH(GPIO_Pin_CFG->GPIO_Port) |= temp_value;
     }
     else
     {
-        temp_value = temp_value << GPIO_CRL_Offset * (GPIO_Pin - 8);
-        switch (GPIOx)
-        {
-        case PA:
-            GPIOA->CRH |= temp_value;
-            break;
-        case PB:
-            GPIOB->CRH |= temp_value;
-            break;
-        case PC:
-            GPIOC->CRH |= temp_value;
-            break;
-        case PD:
-            GPIOD->CRH |= temp_value;
-            break;
-        }
+        return GPIO_InputErr;
     }
+
+    return GPIO_SuccessfulOperation;
 }
 
-void GPIO_Init_Port(u32 GPIOx, u8 GPIO_Mode, u8 GPIO_CFG)
+GPIO_ErrorHandler_Type GPIO_SetPin(GPIO_Port_type GPIO_Port, GPIO_Pin_type GPIO_Pin, GPIO_PinLevelType GPIO_Level)
 {
-    /* Enablicg Clock for GPIOx
-    if (GPIOx == PA)
+    // check if the pin number is valid
+    if (GPIO_Pin > GPIO_PIN15 || GPIO_Pin < GPIO_PIN0)
     {
-        RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
-        RCC->APB2RSTR &= ~(RCC_APB2RSTR_IOPARST);
+        return GPIO_InputErr;
     }
-    else if (GPIOx == PB)
+
+    // check if the port number is valid
+    if (GPIO_Port > GPIO_PORTD || GPIO_Port < GPIO_PORTA)
     {
-        RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
-        RCC->APB2RSTR &= ~(RCC_APB2RSTR_IOPBRST);
+        return GPIO_InputErr;
     }
-    else if (GPIOx == PC)
+
+    if (GPIO_Level == GPIO_PinHIGH)
     {
-        RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
-        RCC->APB2RSTR &= ~(RCC_APB2RSTR_IOPCRST);
+        SET_BIT(GPIO_ODR(GPIO_Port), GPIO_Pin);
     }
-    else if (GPIOx == PD)
+
+    else if (GPIO_Level == GPIO_PinLOW)
     {
-        RCC->APB2ENR |= RCC_APB2ENR_IOPDEN;
-        RCC->APB2RSTR &= ~(RCC_APB2RSTR_IOPDRST);
+        CLEAR_BIT(GPIO_ODR(GPIO_Port), GPIO_Pin);
     }
-*/
-    u32 temp_value = 0x00;
-    temp_value |= GPIO_CFG;
-    temp_value = temp_value << 2;
-    temp_value |= GPIO_Mode;
-    switch (GPIOx)
+
+    else
     {
-    case PA:
-        for (u8 i = 0; i < 8; i++)
-        {
-            temp_value = temp_value << GPIO_CRL_Offset * i;
-            GPIOA->CRL |= temp_value;
-        }
-        for (u8 i = 8; i < 8; i++)
-        {
-            temp_value = temp_value << GPIO_CRL_Offset * i;
-            GPIOA->CRH |= temp_value;
-        }
-        break;
-    case PB:
-        for (u8 i = 0; i < 8; i++)
-        {
-            temp_value = temp_value << GPIO_CRL_Offset * i;
-            GPIOB->CRL |= temp_value;
-        }
-        for (u8 i = 8; i < 8; i++)
-        {
-            temp_value = temp_value << GPIO_CRL_Offset * i;
-            GPIOB->CRH |= temp_value;
-        }
-        break;
-    case PC:
-        for (u8 i = 0; i < 8; i++)
-        {
-            temp_value = temp_value << GPIO_CRL_Offset * i;
-            GPIOC->CRL |= temp_value;
-        }
-        for (u8 i = 8; i < 8; i++)
-        {
-            temp_value = temp_value << GPIO_CRL_Offset * i;
-            GPIOC->CRH |= temp_value;
-        }
-        break;
-    case PD:
-        for (u8 i = 0; i < 8; i++)
-        {
-            temp_value = temp_value << GPIO_CRL_Offset * i;
-            GPIOD->CRL |= temp_value;
-        }
-        for (u8 i = 8; i < 8; i++)
-        {
-            temp_value = temp_value << GPIO_CRL_Offset * i;
-            GPIOD->CRH |= temp_value;
-        }
-        break;
+        return GPIO_InputErr;
     }
+
+    return GPIO_SuccessfulOperation;
 }
 
-void GPIO_SetPin(u32 GPIOx, u32 GPIO_Pin, u32 GPIO_Value)
+GPIO_ErrorHandler_Type GPIO_GetPin(GPIO_Port_type GPIO_Port, GPIO_Pin_type GPIO_Pin)
 {
 
-    u32 temp_value = 0x01;
-
-    if (GPIO_Value == HIGH)
+    // check if the pin number is valid
+    if (GPIO_Pin > GPIO_PIN15 || GPIO_Pin < GPIO_PIN0)
     {
-        /* Check on the Required PORT Number */
-        temp_value |= ((u32)0x01 << (GPIO_Pin));
-        switch (GPIOx)
-        {
-        case PA:
-            GPIOA->ODR |= temp_value;
-            break;
-        case PB:
-            GPIOB->ODR |= temp_value;
-            break;
-        case PC:
-            GPIOC->ODR |= temp_value;
-            break;
-        case PD:
-            GPIOD->ODR |= temp_value;
-            break;
-        }
+        return GPIO_InputErr;
     }
 
-    else if (GPIO_Value == LOW)
+    // check if the port number is valid
+    if (GPIO_Port > GPIO_PORTD || GPIO_Port < GPIO_PORTA)
     {
-        temp_value &= (~((u32)0x01 << GPIO_Pin));
-        switch (GPIOx)
-        {
-        case PA:
-            GPIOA->ODR &= (~((u32)0x01 << GPIO_Pin));
-            break;
-        case PB:
-            GPIOB->ODR &= (~((u32)0x01 << GPIO_Pin));
-            break;
-        case PC:
-            GPIOC->ODR &= (~((u32)0x01 << GPIO_Pin));
-            break;
-        case PD:
-            GPIOD->ODR &= (~((u32)0x01 << GPIO_Pin));
-            break;
-        }
+        return GPIO_InputErr;
+    }
+
+    uint32_t temp_reg = GPIO_IDR(GPIO_Port);
+
+    // check if the pin is high or low
+    if (READ_BIT(temp_reg, GPIO_Pin) == GPIO_ReadHIGH)
+    {
+        return GPIO_ReadHIGH;
+    }
+    else if (READ_BIT(temp_reg, GPIO_Pin) == GPIO_ReadLOW)
+    {
+        return GPIO_ReadLOW;
+    }
+    else
+    {
+        return GPIO_UnknownErr;
     }
 }
 
-void GPIO_SetPort(u32 GPIOx, u32 GPIO_Value)
+GPIO_ErrorHandler_Type GPIO_TogglePin(GPIO_Port_type GPIO_Port, GPIO_Pin_type GPIO_Pin)
 {
-    switch (GPIOx)
+    // check if the pin number is valid
+    if (GPIO_Pin > GPIO_PIN15 || GPIO_Pin < GPIO_PIN0)
     {
-    case PA:
-        GPIOA->ODR |= GPIO_Value;
-        break;
-    case PB:
-        GPIOB->ODR |= GPIO_Value;
-        break;
-    case PC:
-        GPIOC->ODR |= GPIO_Value;
-        break;
-    case PD:
-        GPIOD->ODR |= GPIO_Value;
-        break;
+        return GPIO_InputErr;
     }
-}
 
-
-
-
-u32 GPIO_GetPin(u32 GPIOx, u32 GPIO_Pin)
-{
-    u32 temp_value = 0x00;
-    switch (GPIOx)
+    // check if the port number is valid
+    if (GPIO_Port > GPIO_PORTD || GPIO_Port < GPIO_PORTA)
     {
-    case PA:
-        temp_value = GPIOA->IDR;
-        break;
-    case PB:
-        temp_value = GPIOB->IDR;
-        break;
-    case PC:
-        temp_value = GPIOC->IDR;
-        break;
-    case PD:
-        temp_value = GPIOD->IDR;
-        break;
+        return GPIO_InputErr;
     }
-    temp_value = temp_value >> GPIO_Pin;
-    temp_value &= (u32)0x01;
-    return temp_value;
+
+    TOGGLE_BIT(GPIO_ODR(GPIO_Port), GPIO_Pin);
+
+    return GPIO_SuccessfulOperation;
 }
-
-u32 GPIO_GetPort(u32 GPIOx)
-{
-    u32 temp_value = 0x00;
-    switch (GPIOx)
-    {
-    case PA:
-        temp_value = GPIOA->IDR;
-        break;
-    case PB:
-        temp_value = GPIOB->IDR;
-        break;
-    case PC:
-        temp_value = GPIOC->IDR;
-        break;
-    case PD:
-        temp_value = GPIOD->IDR;
-        break;
-    }
-    return temp_value;
-}
-
-void GPIO_TogglePort(u32 GPIOx)
-{
-    switch (GPIOx)
-    {
-    case PA:
-        GPIOA->ODR ^= 0xFFFFFFFF;
-        break;
-    case PB:
-        GPIOB->ODR ^= 0xFFFFFFFF;
-        break;
-    case PC:
-        GPIOC->ODR ^= 0xFFFFFFFF;
-        break;
-    case PD:
-        GPIOD->ODR ^= 0xFFFFFFFF;
-        break;
-    }
-}
-
-void GPIO_TogglePin(u32 GPIOx, u32 GPIO_Pin)
-{
-    switch (GPIOx)
-    {
-    case PA:
-        GPIOA->ODR ^= ((u32)0x01 << GPIO_Pin);
-        break;
-    case PB:
-        GPIOB->ODR ^= ((u32)0x01 << GPIO_Pin);
-        break;
-    case PC:
-        GPIOC->ODR ^= ((u32)0x01 << GPIO_Pin);
-        break;
-    case PD:
-        GPIOD->ODR ^= ((u32)0x01 << GPIO_Pin);
-        break;
-    }
-}
-
-
 
 /******************************** Software Interface Implementation End *****************************/
--- _src/gpio/private_gpio.h
+++ _src/gpio/private_gpio.h
@@ -12,6 +12,7 @@
 #define __GPIO_H
 
 /******************************** Includes Start **************************************************/
+#include <stdint.h>
 /******************************** Includes End ****************************************************/
 
 /******************************** Macro Declarations Start ****************************************/
@@ -20,108 +21,70 @@
 #define GPIO_BASE_ADDRESS 0x40010800
 #define GPIO_Offset 0x400
 #define GPIO_CRL_Offset 4
+#define GPIO_REG_Offset 0x04
 
 /*GPIO Registers */
-#define GPIOA_BASE_ADDRESS (GPIO_BASE_ADDRESS + GPIO_Offset * 0)
-#define GPIOB_BASE_ADDRESS (GPIO_BASE_ADDRESS + GPIO_Offset * 1)
-#define GPIOC_BASE_ADDRESS (GPIO_BASE_ADDRESS + GPIO_Offset * 2)
-#define GPIOD_BASE_ADDRESS (GPIO_BASE_ADDRESS + GPIO_Offset * 3)
+#define GPIOA_BASE_ADDRESS (GPIO_BASE_ADDRESS + GPIO_Offset * GPIO_PORTA)
+#define GPIOB_BASE_ADDRESS (GPIO_BASE_ADDRESS + GPIO_Offset * GPIO_PORTB)
+#define GPIOC_BASE_ADDRESS (GPIO_BASE_ADDRESS + GPIO_Offset * GPIO_PORTC)
+#define GPIOD_BASE_ADDRESS (GPIO_BASE_ADDRESS + GPIO_Offset * GPIO_PORTD)
 
 /*GPIO Structures*/
-#define GPIOA ((GPIO_TypeDef *)GPIOA_BASE_ADDRESS)
-#define GPIOB ((GPIO_TypeDef *)GPIOB_BASE_ADDRESS)
-#define GPIOC ((GPIO_TypeDef *)GPIOC_BASE_ADDRESS)
-#define GPIOD ((GPIO_TypeDef *)GPIOD_BASE_ADDRESS)
+#define GPIOA_Type ((GPIO_TypeDef *)GPIOA_BASE_ADDRESS)
+#define GPIOB_Type ((GPIO_TypeDef *)GPIOB_BASE_ADDRESS)
+#define GPIOC_Type ((GPIO_TypeDef *)GPIOC_BASE_ADDRESS)
+#define GPIOD_Type ((GPIO_TypeDef *)GPIOD_BASE_ADDRESS)
 
-/*GPIO Modes*/
-#define GPIO_MODE_INPUT 0
-#define GPIO_MODE_OUTPUT_10_MHz 1
-#define GPIO_MODE_OUTPUT_2_MHz 2
-#define GPIO_MODE_OUTPUT_50_MHz 3
-#define GPIO_MODE_OUTPUT 3 // GPIO Output with MAX speed
-
-/*GPIO INPUT Configurations*/
-#define GPIO_CFG_INPUT_Analog_mode 0 // input mode analog
-#define GPIO_CFG_INPUT_Floating 1    // input mode floating
-#define GPIO_CFG_INPUT_PUPD 2        // input mode pull up / pull down
-
-/*GPIO OUTPUt Configurations*/
-#define GPIO_CFG_OUTPUT_PP 0 // output mode push pull
-#define GPIO_CFG_OUTPUT_OD 1 // output mode open drain
-#define GPIO_CFG_AF_PP 2     // alternate function push pull
-#define GPIO_CFG_AF_OD 3     // alternate function open drain
-
-/*GPIO Values*/
-#define LOW 0
-#define HIGH 1
-
-/*GPIO Pins*/
-#define PIN0 0
-#define PIN1 1
-#define PIN2 2
-#define PIN3 3
-#define PIN4 4
-#define PIN5 5
-#define PIN6 6
-#define PIN7 7
-#define PIN8 8
-#define PIN9 9
-#define PIN10 10
-#define PIN11 11
-#define PIN12 12
-#define PIN13 13
-#define PIN14 14
-#define PIN15 15
-
-/*GPIO Ports*/
-#define PA 0
-#define PB 1
-#define PC 2
-#define PD 3
+/*port base addresses*/
+#define GPIOA GPIOA_BASE_ADDRESS
+#define GPIOB GPIOB_BASE_ADDRESS
+#define GPIOC GPIOC_BASE_ADDRESS
+#define GPIOD GPIOD_BASE_ADDRESS
 
 /******************************** Macro Declarations End ******************************************/
 
 /******************************** Macro Function Declarations Start *******************************/
+
+// GPIO Registers
+#define GPIO_CRL(GPIO_PORT) (*((volatile uint32_t *)((GPIO_PORT) + GPIO_REG_Offset * 0)))
+#define GPIO_CRH(GPIO_PORT) (*((volatile uint32_t *)((GPIO_PORT) + GPIO_REG_Offset * 1)))
+#define GPIO_IDR(GPIO_PORT) (*((volatile uint32_t *)((GPIO_PORT) + GPIO_REG_Offset * 2)))
+#define GPIO_ODR(GPIO_PORT) (*((volatile uint32_t *)((GPIO_PORT) + GPIO_REG_Offset * 3)))
+#define GPIO_BSRR(GPIO_PORT) (*((volatile uint32_t *)((GPIO_PORT) + GPIO_REG_Offset * 4)))
+#define GPIO_BRR(GPIO_PORT) (*((volatile uint32_t *)((GPIO_PORT) + GPIO_REG_Offset * 5)))
+#define GPIO_LCKR(GPIO_PORT) (*((volatile uint32_t *)((GPIO_PORT) + GPIO_REG_Offset * 6)))
+
+// Bit Manipulation
+
+#define SET_Value(REG, Value) ((REG) |= Value)
+#define CLEAR_Value(REG, Value) ((REG) &= (~Value))
+#define SET_BIT(REG, BIT) ((REG) |= ((uint32_t)0x01 << (BIT)))
+#define CLEAR_BIT(REG, BIT) ((REG) &= (~((uint32_t)0x01 << BIT)))
+#define TOGGLE_BIT(REG, BIT) ((REG) ^= ((uint32_t)0x01 << BIT))
+#define READ_BIT(REG, BIT) ((REG) &= (((uint32_t)0x01 << BIT)))
+
 /******************************** Macro Function Declarations End *********************************/
 
 /******************************** Data Type Declarations Start ************************************/
 
-/* Standard Data Types*/
-typedef unsigned char u8;
-typedef unsigned short int u16;
-typedef unsigned long int u32;
-typedef signed char s8;
-typedef signed short int s16;
-typedef signed long int s32;
-typedef float f32;
-typedef double f64;
+
+
 
 /*STM32 GPIO Registers Datatypes*/
 typedef struct
 {
-    volatile u32 CRL;
-    volatile u32 CRH;
-    volatile u32 IDR;
-    volatile u32 ODR;
-    volatile u32 BSRR;
-    volatile u32 BRR;
-    volatile u32 LCKR;
+    volatile uint32_t CRL;
+    volatile uint32_t CRH;
+    volatile uint32_t IDR;
+    volatile uint32_t ODR;
+    volatile uint32_t BSRR;
+    volatile uint32_t BRR;
+    volatile uint32_t LCKR;
 } GPIO_TypeDef;
+
+
 
 /******************************** Data Type Declarations End **************************************/
 
-/******************************** Software Interface Declarations Start ***************************/
-void GPIO_Init(u32 GPIOx, u32 GPIO_Pin, u8 GPIO_Mode, u8 GPIO_CFG); // Intiate  GPIO functionality
-void GPIO_Init_Port(u32 GPIOx, u8 GPIO_Mode, u8 GPIO_CFG);         // Intiate  GPIO Port functionality
-
-void GPIO_SetPin(u32 GPIOx, u32 GPIO_Pin, u32 GPIO_Value); // Set GPIO Pin Output Value HIGH, LOW
-void GPIO_SetPort(u32 GPIOx, u32 GPIO_Value);              // Set GPIO Port Output Value HIGH, LOW
-
-u32 GPIO_GetPin(u32 GPIOx, u32 GPIO_Pin);                           // Get GPIO Pin Input Value
-u32 GPIO_GetPort(u32 GPIOx);                                        // Get GPIO Port Input Value
-
-void GPIO_TogglePin(u32 GPIOx, u32 GPIO_Pin);                       // Toggle GPIO Pin Output Value
-void GPIO_TogglePort(u32 GPIOx);                                    // Toggle GPIO Port Output Value
-/******************************** Software Interface Declarations End *****************************/
 
 #endif /* __GPIO_H*/
